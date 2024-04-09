5475c881c278ea5dff5709d6714f2edf8da0d2a0
amandasaporiti <amandasaporiti@gmail.com>
2024-04-09 16:55:04 -0300

    feat: extend colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: {
+          500: "#F48F56",
+        },
+        slate: {
+          850: "#00292E"
+        }
+      }
+    },
   },
   plugins: [],
 }
