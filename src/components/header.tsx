2fe6e2fecdee18df60e96c083dbe438f01cf6a5c
amandasaporiti <amandasaporiti@gmail.com>
2024-04-09 00:47:51 -0300

    feat: create Header Component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,17 @@
 import logo from '../assets/passin-logo.svg'
+import { NavLink } from 'react-router-dom'
 
 export function Header() {
   return (
     <header className="flex items-center gap-5 py-2">
       <img src={logo} alt="" />
       <nav className="flex gap-5 font-medium text-base">
-        <a className="text-gray-300">Eventos</a>
-        <a>Participantes</a>
+        <NavLink to="/" className="text-zinc-400">
+          Eventos
+        </NavLink>
+        <NavLink to="/participants" className="text-zinc-400">
+          Participantes
+        </NavLink>
       </nav>
     </header>
   )
