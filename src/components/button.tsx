442ac566b241f78fdc1db1794eb6f8c784e6e9dd
amandasaporiti <amandasaporiti@gmail.com>
2024-04-18 16:57:04 -0300

    feat: changed variant name default

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,4 +1,4 @@
-import { ComponentProps } from 'react'
+import { ComponentProps, forwardRef } from 'react'
 import { VariantProps, tv } from 'tailwind-variants'
 
 const button = tv({
@@ -7,21 +7,20 @@ const button = tv({
   ],
   variants: {
     variant: {
-      primary:
-        'bg-orange-500 hover:bg-orange-400 py-2 px-4 gap-2 text-slate-850',
+      default:
+        'bg-orange-500 text-slate-850 py-2 px-4 gap-2 hover:bg-orange-400',
       outline:
-        'p-1.5 bg-slate-800 border border-white/10 hover:bg-zinc-900 disabled:bg-zinc-900',
+        'bg-slate-800 border border-white/10 p-1.5 hover:bg-zinc-900 disabled:bg-zinc-900',
     },
   },
 })
 
 type ButtonProps = ComponentProps<'button'> & VariantProps<typeof button>
 
-export function Button({ variant, ...props }: ButtonProps) {
-  return <button {...props} className={button({ variant })} />
-}
-
-// className="bg-white/10 border-white/10 hover:bg-white/5"
-// className="bg-white/10 border-white/10 hover:bg-white/5"
-// variável base = estilizações base, que todos os meus botões irão ter por padrão
-// variants: as customizações
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ variant, className, ...props }, ref) => {
+    return (
+      <button {...props} className={button({ variant, className })} ref={ref} />
+    )
+  },
+)
