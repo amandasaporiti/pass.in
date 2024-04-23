6d56a40f1dee66f6b99b113178b4c70e09b9eb50
amandasaporiti <amandasaporiti@gmail.com>
2024-04-23 14:30:18 -0300

    chore: Remove index.tsx from Table Component

--- src/components/Table/index.tsx
+++
@@ -1,7 +0,0 @@
-import { ComponentProps } from 'react'
-
-type TableProps = ComponentProps<'table'>
-
-export function Table({ ...props }: TableProps) {
-  return <table className="w-full" {...props} />
-}
