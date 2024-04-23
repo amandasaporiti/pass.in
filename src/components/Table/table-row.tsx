16938146e5f10cca2e95e445ec75d1e31c895088
amandasaporiti <amandasaporiti@gmail.com>
2024-04-23 14:05:50 -0300

    style: removed hover style

--- src/components/Table/table-row.tsx
+++ src/components/Table/table-row.tsx
@@ -3,7 +3,5 @@ import { ComponentProps } from 'react'
 type TableRowProps = ComponentProps<'tr'>
 
 export function TableRow({ ...props }: TableRowProps) {
-  return (
-    <tr className="border-b border-white/10 hover:bg-zinc-900" {...props} />
-  )
+  return <tr className="border-b border-white/10" {...props} />
 }
