d37dac8b20ff5294d78806670355d29ac44a67b5
amandasaporiti <amandasaporiti@gmail.com>
2024-04-23 14:10:07 -0300

    refactor: removed nav component, leaving logo as main link to home page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,18 +1,12 @@
 import logo from '../assets/passin-logo.svg'
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export function Header() {
   return (
     <header className="flex items-center gap-5 py-2">
-      <img src={logo} alt="" />
-      <nav className="flex gap-5 font-medium text-base">
-        <NavLink to="/" className="text-zinc-400">
-          Eventos
-        </NavLink>
-        <NavLink to="/participants" className="text-zinc-400">
-          Participantes
-        </NavLink>
-      </nav>
+      <Link to="/eventos">
+        <img src={logo} alt="Página de eventos" />
+      </Link>
     </header>
   )
 }
